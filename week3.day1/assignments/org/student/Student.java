5713639ef1ded32fc85b65c2b9fb07ca61b3faf4
IndhuMP <mohanpon44@gmail.com>
2022-07-27 12:28:29 +0530

    Create Student.java

---
+++ week3.day1/assignments/org/student/Student.java
@@ -0,0 +1,35 @@
+package org.college;
+
+public class Student extends Department {
+	
+	public String studentName()
+	{
+		return "Indhu";
+	}
+
+	public String studentDept()
+	{
+		return deptName();
+	}
+	
+	public int studentId()
+	{
+		return 105;
+	}
+
+	public static void main(String[] args) {
+		
+		Student st = new Student();
+		
+		System.out.println(st.collegeCode());
+		System.out.println(st.collegeName());
+		System.out.println(st.collegeRank());
+		
+		System.out.println(st.studentName());
+		System.out.println(st.studentDept());
+		System.out.println(st.studentId());
+		
+		
+	}
+
+}
