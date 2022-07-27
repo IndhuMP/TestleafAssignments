929df1cbb8ccd39b07a70c3742b6b14b7a1d3b3a
IndhuMP <mohanpon44@gmail.com>
2022-07-27 12:26:57 +0530

    Create College.java

---
+++ week3.day1/assignments/org/college/College.java
@@ -0,0 +1,17 @@
+package org.college;
+
+public class College {
+	public String collegeName()
+	{
+		return "XYZ";
+	}
+	public String collegeCode()
+	{
+		return "XYZ001";
+	}
+	public int collegeRank()
+	{
+		return 15;
+	}
+
+}
