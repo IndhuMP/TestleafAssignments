f0629eb68a6770f2573a9e360a7ecfade7e3b95e
IndhuMP <mohanpon44@gmail.com>
2022-07-27 12:43:35 +0530

    Create BankInfo.java

---
+++ week3.day1/assignments/bank/BankInfo.java
@@ -0,0 +1,19 @@
+package week3.day1.assignment;
+
+//create a class with three methods
+public class BankInfo {
+	
+	public void saving()
+	{
+		System.out.println("Savings account initiated!.");
+	}
+	public void fixed()
+	{
+		System.out.println("Avail fixed deposits here");
+	}
+	public void deposit()
+	{
+		System.out.println("There are 2 types of deposits available.");
+	}
+
+}
