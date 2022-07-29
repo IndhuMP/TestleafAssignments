b4663c08ba7aed7cace9086c4c0f28eee0fe12fe
IndhuMP <mohanpon44@gmail.com>
2022-07-29 12:15:31 +0530

    Create FirstMostDuplicate.java

---
+++ week3.day2/FirstMostDuplicate.java
@@ -0,0 +1,33 @@
+package week3.day2.assignment;
+
+import java.util.*;
+
+public class FirstMostDuplicate {
+
+	public static void main(String[] args) {
+
+		//take an input
+		String input = "abbaba";
+		
+		char[] ch = input.toCharArray();
+		char maxOccChar = input.charAt(0);
+		
+		//create hashmap and put the values of input into it
+		Map<Character, Integer> map = new LinkedHashMap<Character, Integer>();
+		
+		//Keep comparing the occurance with other values -> max value -> character
+		int maxOccur = 0;
+		for(int i=0; i<ch.length; i++)
+		{
+			map.put(ch[i], map.getOrDefault(ch[i], 0)+1);
+			if(map.get(ch[i]) > maxOccur)
+			{
+				maxOccur = map.get(ch[i]);
+				maxOccChar = ch[i];
+			}
+		}
+		
+		System.out.println(maxOccChar);
+	}
+
+}
